22e8619f3686ec2b723927617732f1c7d2466d8a
Gilang Windu Asmara <gilangwinduasmara2@gmail.com>
2020-12-04 18:46:46 +0700

    dashboard

---
+++ dashboard.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+    </script>
+    <script src="https://kit.fontawesome.com/3b79ccf7db.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>V-Zelenny</title>
+</head>
+
+<body>
+    <div class="d-flex">
+        <div class="vh-100 side-menu-container">
+            <div class="side-menu-title">
+                <h4>v-zelenny</h4>
+            </div>
+            <div class="side-menu-item row align-items-center">
+                <i class="fas fa-home col-2"></i>
+                <a class="col-2" href="#">Dashboard</a>
+            </div>
+            <div class="side-menu-item row align-items-center">
+                <i class="fas fa-money-check col-2"></i>
+                <a class="col-2" href="#">Transaksi</a>
+            </div>
+            <div class="side-menu-item row align-items-center">
+                <i class="fas fa-history col-2"></i>
+                <a class="col-2" href="#">Histori</a>
+            </div>
+        </div>
+        <div class="col container-fluid content">
+            <div class="alert alert-primary" role="alert">
+                Halo, kasir1
+            </div>
+            <div class="row">
+                <div class="col-4">
+                    <div class="card card-summary">
+                        <div class="row align-items-center">
+                            <div class="col">
+                                <i class="fas fa-money-check col-2" style="font-size: 6em"></i>
+                            </div>
+                            <div class="col d-flex flex-column align-items-center">
+                                <div class="card-summary-count">
+                                    20
+                                </div>
+                                <div class="card-summary-title">
+                                    Transaksi
+                                </div>
+                            </div>    
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
